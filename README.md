157979757ad95d65eaf65dc490fd86731ac1741d
Shah Nawaz Shuvo <shahnawaz.shuvo@mulytic.io>
2020-04-12 22:40:05 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Identicon
+
+Small Elixir program that generates and stores identicons.
+
+## Installation
+
+Clone the repo and in the project directory run:
+```bash
+$ mix deps.get
+```
+
+Then to run the project:
+```bash
+$ iex -S mix
+```
+
+Once in the iex console run:
+```bash
+iex> Identicon.generate(<input_string>)
+```
+This will generate an identicon based on the input_string and will persist the image in the `priv` directory with the filename same as tje input string.
---
+++ lib/identicon.ex
@@ -0,0 +1,87 @@
+defmodule Identicon do
+  @moduledoc """
+    This module contains the functions that are used to generate and
+    store an identicon image based on an input string.
+  """
+  alias Identicon.Image
+
+  @doc """
+    This is the main funtion that generates an identicon based on the input
+    sting and saves the .png image on the drive.
+
+  ## Examples
+
+      iex> Identicon.generate("programmer")
+      iex> :ok
+
+  """
+  def generate(input) do
+    input
+    |> hash_input
+    |> pick_color
+    |> build_grid
+    |> filter_odd_squares
+    |> build_pixel_map
+    |> draw_image
+    |> save_image(input)
+  end
+
+  defp save_image(image, filename) do
+    File.write("priv/#{filename}.png", image)
+  end
+
+  defp draw_image(%Image{color: color, pixel_map: pixel_map}) do
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
+
+    Enum.each pixel_map, fn({start, stop}) ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end
+
+    :egd.render(image)
+  end
+
+  defp build_pixel_map(%Image{grid: grid} = image) do
+      pixel_map = Enum.map grid, fn({_, index}) ->
+        horizontal = rem(index, 5) * 50
+        vertical = div(index, 5) * 50
+
+        top_left = {horizontal, vertical}
+        bottom_right = {horizontal + 50, vertical + 50}
+
+        {top_left, bottom_right}
+      end
+      %Image{ image | pixel_map: pixel_map }
+  end
+
+  defp filter_odd_squares(%Image{grid: grid} = image) do
+    grid = Enum.filter grid, fn({code, _}) ->
+      rem(code, 2) == 0
+    end
+    %Image{ image | grid: grid }
+  end
+
+  defp build_grid(%Image{hex: hex} = image) do
+    grid =
+      hex
+      |> Enum.chunk_every(3, 3, :discard)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+    %Image{ image | grid: grid }
+  end
+
+  defp mirror_row([f, s | _] = row) do
+    row ++ [s, f]
+  end
+
+  defp pick_color(%Image{hex: [r, g, b | _tail]} = image) do
+    %Image{ image | color: {r, g, b} }
+  end
+
+  defp hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+        |>:binary.bin_to_list
+    %Image{ hex: hex }
+  end
+end
